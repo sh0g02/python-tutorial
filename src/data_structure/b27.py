2e264eb3f23bc50394ee6603db06b962dc27457c
sh0g02 <sh0g0.tksk@gmail.com>
2022-12-25 23:58:07 +0900

    ユークリッドの互除法

---
+++ src/data_structure/b27.py
@@ -0,0 +1,15 @@
+def get_greatest_common_divisor(_a, _b):
+    while _a >= 1 and _b >= 1:
+        if _a >= _b:
+            _a = _a % _b
+        else:
+            _b = _b % _a
+
+    if _a != 0:
+        return _a
+    return _b
+
+
+a, b = list(map(int, input().split()))
+gcd = get_greatest_common_divisor(a, b)
+print(a * b // gcd)
